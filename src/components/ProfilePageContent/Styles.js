d0a35cbb4bb240a25ab7aad08893ec5578d41928
mohamedragaey <mragaey@arkdev.net>
2020-11-09 11:20:38 +0200

    Adding profile info.

--- src/components/ProfilePageContent/Styles.js
+++ src/components/ProfilePageContent/Styles.js
@@ -1,20 +1,40 @@
 import {makeStyles} from '@material-ui/core/styles'
 
 export const useStyles = makeStyles((theme) => ({
-  paginationWrapper: {
-    marginBottom: theme.spacing(4),
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center'
+  profilePageContentWrapper: {},
+  profilePageContentTitle: {
+    lineHeight: 1,
+    marginBottom: 0,
+    fontSize: theme.typography.pxToRem(40)
   },
-  noResult: {
+  profilePageContentSubTitle: {
     display: 'flex',
     alignItems: 'center',
-    justifyContent: 'center',
-    margin: theme.spacing(4, 0),
-    padding: theme.spacing(4, 2),
-    background: theme.palette.common.white,
-    boxShadow: `0 0 5px 0 ${theme.palette.grey[400]}`,
-    fontSize: theme.typography.pxToRem(20)
+    color: theme.palette.grey[400],
+    '& .MuiSvgIcon-root': {
+      fontSize: theme.typography.pxToRem(14),
+      marginLeft: theme.spacing(1)
+    }
+  },
+  profilePageContentDescription: {
+    display: 'flex',
+    alignItems: 'flex-start',
+    marginTop: theme.spacing(2),
+    '& .lazy-load-image-background.blur': {
+      flex: '1 0 auto'
+    }
+  },
+  profilePageContentBio: {
+    marginRight: theme.spacing(0),
+    marginLeft: theme.spacing(1),
+    '& p': {
+      margin: theme.spacing(0),
+      fontSize: theme.typography.pxToRem(18),
+      textAlign: 'justify'
+    },
+    [theme.breakpoints.down('xs')]: {
+      marginRight: theme.spacing(0),
+      textAlign: 'justify'
+    }
   }
 }))
--- src/components/ProfilePageContent/index.js
+++ src/components/ProfilePageContent/index.js
@@ -2,29 +2,31 @@ import React from 'react'
 import {Container, Grid} from '@material-ui/core'
 import {useStyles} from './Styles'
 import {SearchLoader} from '../Loader/SearchLoader'
+import RoomIcon from '@material-ui/icons/Room'
+import LazyImage from '../LazyImage'
 
-// bio: "Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.↵↵Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.↵↵Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.↵↵Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.↵↵Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede."
-// category: "Full-stack developer"
-// city: "Sampacho"
-// country: "Argentina"
-// first_name: "Willabella"
-// generalBio: "Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis."
-// id: 1
-// last_name: "Batten"
-// photoUrl: "http://dummyimage.com/100x100.jpg/cc0000/ffffff"
-// skills: [{title: "React"}, {title: "PHP"}]
 const ProfilePageContent = ({profileObj, loading}) => {
   const classes = useStyles()
 
   return (
-    <Container maxWidth={false}>
+    <Container maxWidth={'lg'}>
       <Grid container spacing={2} justify={'space-between'}>
         {!!loading && <SearchLoader/>}
         {!!Object.keys(profileObj).length &&
         <Grid item xs={12}>
-          <h1>{`${profileObj.first_name} ${profileObj.last_name}`}</h1>
-          {profileObj.id}
-          {profileObj.bio}
+          <div className={classes.profilePageContentWrapper}>
+            <h1 className={classes.profilePageContentTitle}>{`${profileObj.first_name} ${profileObj.last_name}`}</h1>
+            <div className={classes.profilePageContentSubTitle}>
+              {`${profileObj.skills[0].title}, ${profileObj.category}`}
+              <RoomIcon/>{`${profileObj.city}, ${profileObj.country}`}
+            </div>
+            <div className={classes.profilePageContentDescription}>
+              <LazyImage src={profileObj.photoUrl} alt={profileObj.first_name} width={100} height={100}/>
+              <div className={classes.profilePageContentBio}>
+                <p>{profileObj.bio.replace(/^\s+|\s+$/gm, '')}</p>
+              </div>
+            </div>
+          </div>
         </Grid>
         }
       </Grid>
