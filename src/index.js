8217742ef99d3d024fbb382603950f707f8e873e
mohamedragaey <mragaey@arkdev.net>
2020-11-14 23:27:23 +0200

    Adding Service worker.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import ThemeProvider from './ThemeProvider'
 import Layout from './components/Layout'
-import * as serviceWorker from './serviceWorker'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import {BrowserRouter as Router} from 'react-router-dom'
 import {IntlProvider} from 'react-intl'
 import Configuration from './Services/Api/Configuration'
 import {GeneralProvider} from './GeneralContext'
+import reportWebVitals from './reportWebVitals'
 
 let {DefaultLocale, DefaultMessage} = Configuration
 
@@ -30,4 +31,12 @@ ReactDOM.render(
   document.getElementById('root')
 )
 
-serviceWorker.register()
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register()
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
